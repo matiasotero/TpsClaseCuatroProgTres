94edb2ca7966d1f38ffd5b6a384c929da8c90cf9
matiasotero <matiotero_1990@hotmail.com>
2016-04-10 23:09:14 -0300

    termino de hacer todos los metodos de fabrica

--- fabrica.php
+++ fabrica.php
@@ -11,26 +11,35 @@
 	function AgregarEmpleado($empleado)
 	{
 		array_push($this->_empleados, $empleado);
+		EliminarEmpleadosRepetidos();
 		return true;
 	}
 
 	function CalcularSueldos()
 	{
-
+		$totalSueldos;
+		foreach ($this->_empleados as $empleado) 
+		{
+			$totalSueldos+=$this->_empleados[0];
+		}
+		return $totalSueldos;
 	}
 
 	function EliminarEmpleado($persona)
 	{
-
+		unset($persona);
 	}
 
-	private function ElimiarEmpleadosRepetidos()
+	private function EliminarEmpleadosRepetidos()
 	{
-
+		array_unique($this->_empleados);
 	}
 
 	function ToString()
 	{
-
+		foreach ($this->_empleados as $empleado) 
+		{
+			echo $empleado->ToString();
+		}
 	}
 ?>
