f070a363a9bff2a956a0b569a1bccfac821abffc
matiasotero <matiotero_1990@hotmail.com>
2016-04-10 21:38:07 -0300

    termino las clases persona y empleado

--- empleado.php
+++ empleado.php
@@ -5,16 +5,39 @@
 </head>
 <body>
 <?php
-	protected $_legajo;
-	protected $_sueldo;
-
-	function __contruct($nombre, $apellido, $dni, $sexo,$legajo,$sueldo)
+	include_once "persona.php";
+	class Empleado extends Persona 
 	{
-		//parent::__construct($nombre, $apellido, $dni, $sexo);
-		$_legajo=$legajo;
-		$_sueldo=$sueldo;
-	}		
+		protected $_legajo;
+		protected $_sueldo;
 
+		function __contruct($nombre, $apellido, $dni, $sexo,$legajo,$sueldo)
+		{
+			parent::__construct($nombre, $apellido, $dni, $sexo);
+			$_legajo=$legajo;
+			$_sueldo=$sueldo;
+		}	
+
+		function getLegajo()
+		{
+			return $_legajo;
+		}	
+
+		function getSueldo()
+		{
+			return $_sueldo;
+		}
+
+		function Hablar($idioma)
+		{
+			return $idioma;
+		}
+
+		function ToString()
+		{			
+			return parent::ToString()."Legajo: ".getLegajo()."<br>"."Sueldo: ".getSueldo()."<br>";			
+		}
+	}
 ?>
 </body>
 </html>
--- persona.php
+++ persona.php
@@ -12,19 +12,43 @@
 		private $_nombre;
 		private $_sexo;
 
-		function __construct($nombre, $apellido, $dni, $sexo);
+		function __construct($nombre, $apellido, $dni, $sexo)
+		{
+			$_nombre=$nombre;
+			$_apellido=$apellido;
+			$_dni=$dni;
+			$_sexo=$sexo;
+		}
 
-		function getApellido();
+		function getApellido()
+		{
+			return $_apellido;
+		}
 
-		function getDni();
+		function getDni()
+		{
+			return $_dni;
+		}
 
-		function getNombre();
+		function getNombre()
+		{
+			return $_nombre;
+		}
 
-		function getSexo();
+		function getSexo()
+		{
+			return $_sexo;
+		}
 
-		function Hablar($idioma);
+		function Hablar($idioma)
+		{
+			return $idioma;
+		}
 
-		function ToString();
+		function ToString()
+		{
+			return "Apellido: ".getApellido()."<br>"."Nombre: ".getNombre()."<br>"."Dni: ".getDni()."<br>"."Sexo: ".getSexo()."<br>";
+		}
 	}
 ?>
 </body>
